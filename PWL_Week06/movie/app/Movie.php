38e5d07bdd3f9abd649fa2445202a16b9763d7df
NdusFTI <143603135+NdusFTI@users.noreply.github.com>
2025-10-27 16:59:34 +0700

    feat: week 8 pt 3

---
+++ PWL_Week06/movie/app/Movie.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    protected $table = 'movie';
+
+    protected $fillable = [
+        'imdb_id',
+        'title',
+        'genre',
+        'year',
+        'description',
+        'poster'
+    ];
+}
